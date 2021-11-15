744a82b9f7a8284c27634e0065985b192d17ac1c
M J <tana12programing@gmail.com>
2021-11-15 09:47:11 +0000

    create database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_09_054726) do
+ActiveRecord::Schema.define(version: 2021_11_14_064852) do
 
   create_table "tasks", charset: "utf8mb4", force: :cascade do |t|
     t.string "content"
